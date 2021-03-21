e8a777815322056c2d3b80ae51940d09225f8c44
fabio98p <parisifabio98@gmail.com>
2021-03-21 22:08:51 +0100

    fine milestone 3

--- js-snack-cats/main.js
+++ js-snack-cats/main.js
@@ -47,65 +47,61 @@ $(document).ready(() => {
 		$(".Milestone-1 .colorName").append(`<li>il gattino ${element.nome} è di colore ${element.colore}</li>`)
 	});
 	// Milestone 2
-	// Dividere i gatti in due contenitori distinti in base al sesso e aggiungere
+	//Dividere i gatti in due contenitori distinti in base al sesso e aggiungere
 	//a fianco di ogni gattino un fiocco colorato di rosa, se femmina, o di blu, se maschio.
 	//Il colore del fiocco deve essere più tenue se il gatto è più
 	//giovane, più scuro se il gatto è più vecchio.
-	
-	
-	
-	cats.forEach(function (e, i) {
-		$(".Milestone-2 .fiocchetto").append(`<li>il gattino ${e.nome} è di colore ${e.colore}</li>`)
-		let selezione = $(".Milestone-2 .fiocchetto li")[i]
-		opacity(e.eta, e.sesso, selezione)
-	});
-	
-	
+
 	const femmine = cats.filter(element => element.sesso == "femmina");
 	const maschi = cats.filter(element => element.sesso == "maschio");
-	console.log(femmine, maschi);
-	
-	
-	cats.filter((element, index, array) => {
-		if (element.sesso == "femmina") {
-			femmine.push(element)			
-		}else{
-			maschi.push(element)
-		}
-	})
 
-
+	cats.forEach((e, i) => {
+		$(".Milestone-2 .fiocchetto").append(`<li>il gattino ${e.nome}</li>`)
+		let selezione = $(".Milestone-2 .fiocchetto li")[i]
+		fiocco(e.eta, e.sesso, selezione)
+	});
 
 	// Milestone 3
 	// Creare un nuovo array con prima tutti i gattini femmina e poi tutti i
 	//gattini maschio, inserendo solamente nome e colore e colore e opacità del
 	//fiocco per ogni gatto.
 
+	const femmineMaschi = [...femmine, ...maschi]
 
-	
-	function opacity(eta, sesso, e) {
+	femmineMaschi.forEach((e, i) => {
+		$(".Milestone-3 .femmineMaschi").append(`<li>${e.nome} ${e.colore}</li>`)
+		let selezione = $(".Milestone-3 .femmineMaschi li")[i]
+		fiocco(e.eta, e.sesso, selezione)
+	});
+
+	//FUNZIONI
+	function fiocco(eta, sesso, e) {
 		console.log(eta, sesso, e);
 		$(e).append(`<i class="fas fa-gift"></i>`)
-		$(e).children("i").css({"opacity": ()=> {
-			if (eta < 3) {
-				console.log(eta);
-				return 0.2
-			}else if (eta > 3 && eta < 6) {
-				console.log(eta);
-				return 0.5
-			}else if (eta >= 6 && eta < 10) {
-				console.log(eta);
-				return 0.8
-			}else if (eta >= 10) {
-				console.log(eta);
-				return 1
+		$(e).children("i").css({
+			"opacity": () => {
+				if (eta < 3) {
+					console.log(eta);
+					return 0.2
+				} else if (eta > 3 && eta < 6) {
+					console.log(eta);
+					return 0.5
+				} else if (eta >= 6 && eta < 10) {
+					console.log(eta);
+					return 0.8
+				} else if (eta >= 10) {
+					console.log(eta);
+					return 1
+				}
 			}
-		}}).css({"color": () => {
-			if (sesso == "maschio") {
-				return "blue"
-			} else if (sesso == "femmina") {
-				return "pink"
+		}).css({
+			"color": () => {
+				if (sesso == "maschio") {
+					return "blue"
+				} else if (sesso == "femmina") {
+					return "pink"
+				}
 			}
-		}})
+		})
 	}
 })
