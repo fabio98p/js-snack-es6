3f61730b8b9417d1b6372e211c08807cd878878e
fabio98p <parisifabio98@gmail.com>
2021-03-21 21:44:29 +0100

    fine milestone 2

--- js-snack-cats/main.js
+++ js-snack-cats/main.js
@@ -11,31 +11,31 @@ $(document).ready(() => {
 	},
 	{
 		nome: "era",
-		eta: 3,
+		eta: 6,
 		colore: "nero",
 		sesso: "maschio",
 	},
 	{
 		nome: "ebe",
-		eta: 1,
+		eta: 4,
 		colore: "arancione",
 		sesso: "maschio",
 	},
 	{
 		nome: "doraimon",
-		eta: 30,
+		eta: 7,
 		colore: "blu",
 		sesso: "maschio",
 	},
 	{
 		nome: "uranietta",
-		eta: 45,
+		eta: 11,
 		colore: "marrone",
 		sesso: "femmina",
 	},
 	{
 		nome: "beby",
-		eta: 25,
+		eta: 5,
 		colore: "giallo",
 		sesso: "femmina",
 	},
@@ -43,27 +43,69 @@ $(document).ready(() => {
 	//Tramite la funzione .forEach(), stampare in pagina tutti i gattini,
 	//ciascuno con il proprio colore e il proprio nome.
 
-	cats.forEach(items => {
-		$(".colorName").append(`<li>il gattino ${items.nome} è di colore ${items.colore}</li>`)
+	cats.forEach(element => {
+		$(".Milestone-1 .colorName").append(`<li>il gattino ${element.nome} è di colore ${element.colore}</li>`)
 	});
-
-
-
-
-
-
-
 	// Milestone 2
 	// Dividere i gatti in due contenitori distinti in base al sesso e aggiungere
-	//a fianco di ogni gattino un fiocco colorato di rosa, se femmina, o di blu,
-	//se maschio. Il colore del fiocco deve essere più tenue se il gatto è più
+	//a fianco di ogni gattino un fiocco colorato di rosa, se femmina, o di blu, se maschio.
+	//Il colore del fiocco deve essere più tenue se il gatto è più
 	//giovane, più scuro se il gatto è più vecchio.
+	
+	
+	
+	cats.forEach(function (e, i) {
+		$(".Milestone-2 .fiocchetto").append(`<li>il gattino ${e.nome} è di colore ${e.colore}</li>`)
+		let selezione = $(".Milestone-2 .fiocchetto li")[i]
+		opacity(e.eta, e.sesso, selezione)
+	});
+	
+	
+	const femmine = cats.filter(element => element.sesso == "femmina");
+	const maschi = cats.filter(element => element.sesso == "maschio");
+	console.log(femmine, maschi);
+	
+	
+	cats.filter((element, index, array) => {
+		if (element.sesso == "femmina") {
+			femmine.push(element)			
+		}else{
+			maschi.push(element)
+		}
+	})
+
+
+
 	// Milestone 3
-
-
 	// Creare un nuovo array con prima tutti i gattini femmina e poi tutti i
 	//gattini maschio, inserendo solamente nome e colore e colore e opacità del
 	//fiocco per ogni gatto.
 
 
+	
+	function opacity(eta, sesso, e) {
+		console.log(eta, sesso, e);
+		$(e).append(`<i class="fas fa-gift"></i>`)
+		$(e).children("i").css({"opacity": ()=> {
+			if (eta < 3) {
+				console.log(eta);
+				return 0.2
+			}else if (eta > 3 && eta < 6) {
+				console.log(eta);
+				return 0.5
+			}else if (eta >= 6 && eta < 10) {
+				console.log(eta);
+				return 0.8
+			}else if (eta >= 10) {
+				console.log(eta);
+				return 1
+			}
+		}}).css({"color": () => {
+			if (sesso == "maschio") {
+				return "blue"
+			} else if (sesso == "femmina") {
+				return "pink"
+			}
+		}})
+	}
 })
